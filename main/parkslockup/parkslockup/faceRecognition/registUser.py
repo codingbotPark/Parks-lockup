8941ed344ae1659d8d548ef36d25f85fd186dbf3
박병관 <pbk575@gmail.com>
2023-06-19 11:28:07 +0900

    [ADD] clear duty in encoding

--- main/parkslockup/parkslockup/faceRecognition/registUser.py
+++ main/parkslockup/parkslockup/faceRecognition/registUser.py
@@ -1,28 +1,31 @@
 import face_recognition
 from openpyxl import Workbook
 import os
-
+import numpy as np
 
 # 로드할 파일의 파일확장자
 fileExtension = [
     '.jpg',
     '.png',
-    '.jpeg'
+    '.jpeg',
 ]
-# registImgs 는 __init__.py 에 의해 호출된다, 이에따른 경로를 설정
 imgDirPath = "./imgs"
+# registImgs 는 __init__.py 에 의해 호출된다, 이에따른 경로를 설정
 def loadFile(file,dir):
     # .jpg, .png, .jpeg 파일만 로드
     if '.' + file.split('.')[-1] in fileExtension:
         return os.path.join(dir,file)
-registerList = list(map(lambda file:loadFile(file,imgDirPath),os.listdir(imgDirPath)))
-
+    
+registerList = list(filter(None,map(lambda file:loadFile(file,imgDirPath),os.listdir(imgDirPath))))
+print(registerList)
 
 # --------
 
 def registImgs():
     print(registerList)
     encodedImgs = list(map(encodingImg,registerList))
+    # 배열에서 얼굴을 못 찾은, 즉 none값을 없애준다
+    encodedImgs = list(filter(lambda x: isinstance(x,np.ndarray),encodedImgs))
     makeExel(encodedImgs,registerList)
     
 
@@ -34,7 +37,7 @@ def encodingImg(path):
         return EncodedImg[0]
     else:
         print("Cannot find face")
-        return False
+        return None
 
 
 
